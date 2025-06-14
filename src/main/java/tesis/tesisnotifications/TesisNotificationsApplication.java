e1308d0831004e7e0a2f88df91037ea0ed3ad3ab
agustin <114094@tecnicatura.frc.utn.edu.ar>
2025-06-14 20:06:45 -0300

    Commit inicial

---
+++ src/main/java/tesis/tesisnotifications/TesisNotificationsApplication.java
@@ -0,0 +1,13 @@
+package tesis.tesisnotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesisNotificationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesisNotificationsApplication.class, args);
+    }
+
+}
